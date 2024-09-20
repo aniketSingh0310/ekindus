c061a9a60b5ee929dce097538d68b90fcf2f5bea
aniketSingh0310 <aniketfcb10@gmail.com>
2024-09-20 11:20:28 +0530

    ..

--- app/components/hero/index.tsx
+++ app/components/hero/index.tsx
@@ -20,8 +20,8 @@ const Hero = () => {
       <div className="relative z-10 flex flex-col md:flex-row justify-between  w-full h-full px-8 lg:px-24">
         {/* Left Section - Text */}
         <div className="text-white py-[7rem]">
-          <h1 className="text-[3.5rem] font-bold font-[family-name:var(--font-rethink)] leading-[1.1]">
-          The American Dream<br/> Made Simple.
+          <h1 className="text-[3.5rem] font-bold font-[family-name:var(--font-rethink)] leading-[1.1] text-center">
+          The American Dream<br/> Made Simple
           </h1>
           <p className="text-[1rem]  font-[family-name:var(--font-manrope)] mt-4">
           The process of getting an EB-5 visa, made as seamless as possible.
