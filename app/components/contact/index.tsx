56f1ec267640de940801c6ff64d3e63196cf13bb
aniketSingh0310 <aniketfcb10@gmail.com>
2024-09-24 12:21:45 +0530

    , fix

--- app/components/contact/index.tsx
+++ app/components/contact/index.tsx
@@ -4,13 +4,13 @@ import Form2 from "../shared/form2";
 const Contact = () => {
   return (
     <div id="contact" className="w-full ">
-      <p className="text-xs text-[#212121]/70 ml-5 mb-3">
-        *partnership with IMMCO investments, the EB-5 manager, and the Regional
+      <p className="text-xs text-[#212121]/70 ml-5 mb-3 tracking-tight">
+        *partnership with IMMCO investments, the EB-5 manager and the Regional
         Center sponsoring the investment project.
       </p>
-      <p className="text-xs text-[#212121]/70 px-5 mb-3 text-justify">
+      <p className="text-xs text-[#212121]/70 px-5 mb-3 text-justify tracking-tighter">
         ** The figures here represent the track record of IMMCO Investments, the
-        EB-5 manager, and the Regional Center sponsoring the investment projects
+        EB-5 manager and the Regional Center sponsoring the investment projects
         through our strategic partnership. Ek.indUS exists solely for the
         purpose of marketing, promoting, and providing access to IMMCO&apos;s
         EB-5 strategies for the Indian market, and shall not be interpreted
--- app/components/hero/index.tsx
+++ app/components/hero/index.tsx
@@ -1,6 +1,7 @@
+"use client";
 import React from "react";
 import Form from "../shared/form";
-
+import { Fade, Slide } from "react-awesome-reveal";
 const Hero = () => {
   return (
     <div className="relative w-full h-max md:h-[80vh] overflow-hidden hidden md:block mt-[5.2rem]">
@@ -20,12 +21,17 @@ const Hero = () => {
       <div className="relative z-10 flex flex-col md:flex-row justify-between max-w-7xl md:px-[7rem] lg:px-[5rem] xl:px-[3rem] h-full mx-auto">
         {/* Left Section - Text */}
         <div className="text-white py-[7rem] md:pt-[5rem] md:pb-[10rem] flex flex-col justify-center md:translate-y-[-10%] lg:translate-y-0">
-          <h1 className="text-[2rem] md:text-[2.8rem] lg:text-[3.5rem] font-bold font-[family-name:var(--font-rethink)] leading-[1.2] md:leading-[1.1] text-center">
-            The American Dream<br /> Made Simple
-          </h1>
-          <p className="text-[1rem] md:text-[1.2rem] lg:text-[1.2rem] font-[family-name:var(--font-manrope)] mt-4 text-center md:text-left tracking-tight">
-            The process of getting an EB-5 visa, made as seamless as possible.
-          </p>
+          <Slide  duration={1500}>
+            <h1 className="text-[2rem] md:text-[2.8rem] lg:text-[3.5rem] font-bold font-[family-name:var(--font-rethink)] leading-[1.2] md:leading-[1.1] text-center">
+              The American Dream
+              <br /> Made Simple
+            </h1>
+          </Slide>
+          <Fade delay={1} cascade damping={0.1} duration={2000}>
+            <p className="text-[1rem] md:text-[1.2rem] lg:text-[1.2rem] font-[family-name:var(--font-manrope)] mt-4 text-center md:text-left tracking-tight">
+              The process of getting an EB-5 visa, made as seamless as possible.
+            </p>
+          </Fade>
         </div>
 
         {/* Right Section - Contact Form */}
